58aa5a086c0022de0f8b2a0c68e3c4e31ac3e180
Csordás Bence <bence@csorysoft.hu>
2024-10-16 11:22:26 +0200

    fireLaser

--- lab4-main/src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ lab4-main/src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -21,7 +21,23 @@ public class GT4500 implements SpaceShip {
   }
 
   public boolean fireLaser(FiringMode firingMode) {
-    
+    boolean firingSuccess = false;
+
+    switch (firingMode) {
+        boolean firingSuccess = false;
+
+    switch (firingMode) {
+        case SINGLE:
+            
+            firingSuccess = fireSingleLaser();
+            break;
+        case ALL:
+            
+            firingSuccess = fireAllLasers();
+            break;
+    }
+
+    return firingSuccess;
 }
 
 private boolean fireSingleLaser() {
